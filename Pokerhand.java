4566a71447d3fb86b72613105a51db927aa87607
Yichen Liu <yichen_liu@m018110.aw.org>
2018-10-10 21:07:32 -0700

    halfway

--- Pokerhand.java
+++ Pokerhand.java
@@ -1,9 +1,23 @@
+import java.util.*;
+
 public class Pokerhand {
+
+  public Deck shuffle(Deck d){
+    return d;
+  }
 
   public static void main(String[] args) {
 
+    Random r = new Random();
+    // System.out.println(r.nextDouble());
+    // System.out.println(r.nextDouble());
+    int[] ar = r.ints(50, 0, 100).distinct().toArray();
+    for (int i = 0; i < ar.length; i++) {
+      System.out.println(ar[i]);
+    }
+
     Deck d =new Deck();
-    System.out.println(d.cards);
-    
+    //System.out.println(d.cards);
+
   }
 }
